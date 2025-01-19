826f526a0812981b95bd1c73149f9e1dfec44a74
ericFlutterast <erikGitFlutter@gmail.com>
2025-01-19 17:46:33 +0300

    init project

---
+++ Drill gym/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Drill gym
+//
+//  Created by ericFlutter on 19.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
